8a92376fcfaace968e282807825edbc68799e6f8
bosza001 <51392449+bosza001@users.noreply.github.com>
2019-10-02 12:05:58 +0700

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ def callback():
     user = decoded['originalDetectIntentRequest']['payload']['replyToken']
     userText = decoded['queryResult']['intent']['displayName']
     #sendText(user,userText)
-    if(userText == 'สวัสดี') :
+    if(userText == ('สวัสดี') :
        sendText(user('สวัสดี ครับ')
     elif(userText == 'ไปละนะ') :
        sendText(user('บาย')
@@ -31,17 +31,17 @@ def callback():
        sendText(user('ไม่เข้าใจ')   
     return '',200
 
-def sendText(user, text):
-  LINE_API = 'https://api.line.me/v2/bot/message/reply'
-  headers = {
+    def sendText(user, text):
+    LINE_API = 'https://api.line.me/v2/bot/message/reply'
+    headers = {
     'Content-Type': 'application/json; charset=UTF-8',
     'Authorization': os.environ['Authorization']    # ตั้ง Config vars ใน heroku พร้อมค่า Access token
-  }
-  data = json.dumps({
+    }
+    data = json.dumps({
     "replyToken":user,
     "messages":[{"type":"text","text":text}]
-  })
-  r = requests.post(LINE_API, headers=headers, data=data) # ส่งข้อมูล
+    })
+    r = requests.post(LINE_API, headers=headers, data=data) # ส่งข้อมูล
 
-if __name__ == '__main__':
+    if __name__ == '__main__':
     app.run()
